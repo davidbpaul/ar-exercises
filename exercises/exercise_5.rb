f6970414ce4ad3f0138330865172f173f1140759
David Paul <david-paul23@hotmail.com>
2016-11-09 02:42:20 +0000

    employees and stores

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,14 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total_revenue = Store.sum(:annual_revenue)
+average_revenue = Store.average(:annual_revenue)
+puts "Total revenue for the Company: $#{total_revenue}"
+puts "Average revenue for all stores: $#{average_revenue.floor}"
+
+million = Store.where("annual_revenue > ?", [1_000_000])
+
+puts million.count
+million.each do |store|
+  puts "Store Name: #{store.name} Annual Revenue #{store.annual_revenue}"
+end
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,14 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: 'Stacey', last_name: 'red', hourly_rate: 20)
+@store1.employees.create(first_name: 'dif', last_name: 'blue', hourly_rate: 30)
+@store1.employees.create(first_name: 'fs', last_name: 'yellow', hourly_rate: 1)
+
+@store2.employees.create(first_name: 'no', last_name: 'purple', hourly_rate: 3)
+@store2.employees.create(first_name: 'dathvader', last_name: 'Brown', hourly_rate: 54)
+@store2.employees.create(first_name: 'windows', last_name: 'Pink', hourly_rate: 45)
+@store2.employees.create(first_name: 'wall', last_name: 'L', hourly_rate: 30)
+
+puts "Store 1 has #{@store1.employees.count} employees"
+puts "Store 2 has #{@store2.employees.count} employees"
